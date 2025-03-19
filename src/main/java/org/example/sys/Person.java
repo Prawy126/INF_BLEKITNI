04b8e1b21feb7d8b7b145ec8aa0ff2f41de5d8a9
Prawy126 <michal190713@gmail.com>
2025-03-19 09:12:20 +0100

    Podstawowa struktura

---
+++ src/main/java/org/example/sys/Person.java
@@ -0,0 +1,66 @@
+package org.example.sys;
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+    private String password;
+    private String  email;
+
+    public Person(String name, int age, String address, String password, String email) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.password = password;
+        this.email = email;
+    }
+    public Person(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public boolean isPassword(String password){
+        return this.password.equals(password);
+    }
+    public boolean isEmail(String email){
+        return this.email.equals(email);
+    }
+}
