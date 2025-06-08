b6b2f74f53bae309a32d0c1d7cfdb0a995e9edde
JakubOpar <oparkuba2002@gmail.com>
2025-06-08 10:41:03 +0200

    Prace nad instalatorem cd.

--- src/main/java/org/example/pdflib/ConfigManager.java
+++ src/main/java/org/example/pdflib/ConfigManager.java
@@ -8,9 +8,12 @@
 package org.example.pdflib;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.example.utils.AppPaths;
 
 /**
  * Prosty menedżer konfiguracji oparty na pliku properties.
@@ -21,17 +24,58 @@ public class ConfigManager {
     private static final Logger logger = LogManager.getLogger(ConfigManager.class);
     private static Properties props = new Properties();
 
-    public static String getReportPath() {
-        Properties props = new Properties();
-        try (InputStream input = new FileInputStream(CONFIG_FILE)) {
-            props.load(input);
-            String path = props.getProperty("pdf.output.path", "");
-            logger.debug("Ustawiono ścieżkę raportów: {}", path);
-            return path;
+    static {
+        loadProperties();
+    }
+
+    /**
+     * Ładuje właściwości z pliku konfiguracyjnego.
+     */
+    private static void loadProperties() {
+        Path configFile = AppPaths.getConfigFile(CONFIG_FILE);
+
+        try {
+            if (Files.exists(configFile)) {
+                try (InputStream input = Files.newInputStream(configFile)) {
+                    props.load(input);
+                    logger.debug("Wczytano konfigurację z {}", configFile);
+                }
+            } else {
+                // Ustaw domyślne wartości
+                props.setProperty("pdf.output.path", AppPaths.getReportsDirectory().toString());
+                props.setProperty("pdf.logo.path", AppPaths.getResourceFile("logo.png").toString());
+                props.setProperty("logging.enabled", "true");
+
+                saveProperties();
+                logger.info("Utworzono domyślny plik konfiguracyjny w {}", configFile);
+            }
         } catch (IOException e) {
-            logger.error("Nie można wczytać pliku konfiguracyjnego: {}", CONFIG_FILE, e);
-            return "";
+            logger.error("Nie można wczytać pliku konfiguracyjnego: {}", configFile, e);
         }
+    }
+
+    /**
+     * Zapisuje właściwości do pliku konfiguracyjnego.
+     */
+    private static void saveProperties() {
+        Path configFile = AppPaths.getConfigFile(CONFIG_FILE);
+
+        try {
+            Files.createDirectories(configFile.getParent());
+            try (OutputStream output = Files.newOutputStream(configFile)) {
+                props.store(output, "Ustawienia aplikacji");
+                logger.debug("Zapisano konfigurację do {}", configFile);
+            }
+        } catch (IOException e) {
+            logger.error("Nie można zapisać pliku konfiguracyjnego: {}", configFile, e);
+        }
+    }
+
+    public static String getReportPath() {
+        String path = props.getProperty("pdf.output.path",
+                AppPaths.getReportsDirectory().toString());
+        logger.debug("Pobrano ścieżkę raportów: {}", path);
+        return path;
     }
 
     public static boolean isLoggingEnabled() {
@@ -42,76 +86,32 @@ public class ConfigManager {
 
     public static void setLoggingEnabled(boolean on) {
         props.setProperty("logging.enabled", Boolean.toString(on));
-        save();
+        saveProperties();
         logger.info("Logowanie zostało {}.", on ? "włączone" : "wyłączone");
     }
 
-    private static void save() {
-        try (OutputStream out = new FileOutputStream(CONFIG_FILE)) {
-            props.store(out, "Ustawienia aplikacji");
-            logger.debug("Zapisano zmiany w pliku konfiguracyjnym.");
-        } catch (IOException e) {
-            logger.error("Nie można zapisać pliku konfiguracyjnego: {}", CONFIG_FILE, e);
-        }
-    }
-
     public static void setReportPath(String path) {
-        Properties props = new Properties();
-        try {
-            File file = new File(CONFIG_FILE);
-            if (file.exists()) {
-                try (InputStream input = new FileInputStream(file)) {
-                    props.load(input);
-                }
-            }
-
-            props.setProperty("pdf.output.path", path);
-
-            try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
-                props.store(output, null);
-                logger.info("Zaktualizowano ścieżkę wyjściową PDF na: {}", path);
-            }
-
-        } catch (IOException e) {
-            logger.error("Nie można ustawić ścieżki wyjściowej PDF.", e);
-        }
+        props.setProperty("pdf.output.path", path);
+        saveProperties();
+        logger.info("Zaktualizowano ścieżkę wyjściową PDF na: {}", path);
     }
 
     /**
-     * Pobiera ścieżkę do pliku z logo (jeśli nie ma w properties, zwraca domyślną).
+     * Pobiera ścieżkę do pliku z logo.
      */
     public static String getLogoPath() {
-        Properties props = new Properties();
-        try (InputStream input = new FileInputStream(CONFIG_FILE)) {
-            props.load(input);
-            return props.getProperty("pdf.logo.path", "src/main/resources/logo.png");
-        } catch (IOException e) {
-            logger.error("Nie można wczytać pliku konfiguracyjnego: {}", CONFIG_FILE, e);
-            return "src/main/resources/logo.png";
-        }
+        String path = props.getProperty("pdf.logo.path",
+                AppPaths.getResourceFile("logo.png").toString());
+        logger.debug("Pobrano ścieżkę do logo: {}", path);
+        return path;
     }
 
     /**
      * Ustawia ścieżkę do logo i zapisuje ją do config.properties.
      */
     public static void setLogoPath(String path) {
-        Properties props = new Properties();
-        try {
-            File file = new File(CONFIG_FILE);
-            if (file.exists()) {
-                try (InputStream input = new FileInputStream(file)) {
-                    props.load(input);
-                }
-            }
-
-            props.setProperty("pdf.logo.path", path);
-
-            try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
-                props.store(output, null);
-                logger.info("Zaktualizowano ścieżkę do logo PDF na: {}", path);
-            }
-        } catch (IOException e) {
-            logger.error("Nie można ustawić ścieżki do logo PDF.", e);
-        }
+        props.setProperty("pdf.logo.path", path);
+        saveProperties();
+        logger.info("Zaktualizowano ścieżkę do logo PDF na: {}", path);
     }
 }
